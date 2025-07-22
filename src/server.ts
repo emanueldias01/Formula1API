e7730e5b28aa6ac842757302ed8debc321d04b3e
emanueldias01 <emanueldias01dev@gmail.com>
2025-07-22 20:59:43 -0300

    create api minimal

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+import fastify from "fastify";
+
+
+const server = fastify()
+
+server.get("/teams", async (req, res) => {
+    res.type("application/json");
+    return[{id : 1, team : "Ferrari"}];
+})
+
+server.get("/drivers", async(req, res) => {
+    res.type("applicatio/json");
+    return [{id : 1, name : "Max Verstappen", team : "Red Bull Racing"}]
+})
+
+server.listen({port : 3333}, () => {
+    console.log("app running in 3333");
+})
