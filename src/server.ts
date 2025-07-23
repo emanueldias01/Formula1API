9696edc350918e5958e8bdfaa2162c79cff22b0d
emanueldias01 <emanueldias01dev@gmail.com>
2025-07-22 21:15:45 -0300

    create filters

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,11 @@ import fastify from "fastify";
 
 const server = fastify();
 
+interface DataParams{
+    id : string
+}
+
+
 const teams = [
     {id : 1, name : "McLaren", base : "Woking, United Kingdom"},
     {id : 2, name : "Mercedes", base : "Brackley, United Kingdom"},
@@ -19,10 +24,32 @@ server.get("/teams", async (req, res) => {
     return { teams };
 });
 
-server.get("/drivers", async(req, res) => {
-    res.type("applicatio/json");
+server.get("/drivers", async (req, res) => {
+    res.type("application/json");
     return { drivers };
 });
+
+server.get<{Params: DataParams}>("/drivers/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const driver = drivers.find(d => d.id === id);
+    if(!driver){
+        res.type("application/json").code(404).send({ error: "Driver not found" });;
+    }else{
+        res.type("application/json").code(200);
+        return driver;
+    }
+});
+
+server.get<{Params : DataParams}>("/teams/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const team = teams.find(t => t.id === id);
+    if(!team){
+       res.type("application/json").code(404).send({ error: "Team not found" });; 
+    }else{
+      res.type("application/json").code(200);
+        return team; 
+    }
+})
 
 server.listen({port : 3333}, () => {
     console.log("app running in 3333");
