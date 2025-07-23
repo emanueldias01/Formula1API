6350cbb9497dbc9816aeaeb21a76fc4d29f87618
emanueldias01 <emanueldias01dev@gmail.com>
2025-07-22 21:04:50 -0300

    separate data

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,29 @@
 import fastify from "fastify";
 
+const server = fastify();
 
-const server = fastify()
+const teams = [
+    {id : 1, name : "McLaren", base : "Woking, United Kingdom"},
+    {id : 2, name : "Mercedes", base : "Brackley, United Kingdom"},
+    {id : 3, name : "Red Bull Racing", base : "Milton Keynes, United Kingdom"}
+];
+
+const drivers = [
+    {id : 1, name : "Max Verstappen", team : "Red Bull Racing"},
+    {id : 2, name : "Lewis Hamilton", team : "Ferrari"},
+    {id : 3, name : "Lando Norris", team : "McLaren"}
+];
 
 server.get("/teams", async (req, res) => {
     res.type("application/json");
-    return[{id : 1, team : "Ferrari"}];
-})
+    return { teams };
+});
 
 server.get("/drivers", async(req, res) => {
     res.type("applicatio/json");
-    return [{id : 1, name : "Max Verstappen", team : "Red Bull Racing"}]
-})
+    return { drivers };
+});
 
 server.listen({port : 3333}, () => {
     console.log("app running in 3333");
-})
+});
