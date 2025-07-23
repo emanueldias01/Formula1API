1543e4aa017981c7a2547d255190c61255972ca2
emanueldias01 <emanueldias01dev@gmail.com>
2025-07-22 21:18:10 -0300

    implements data

--- src/server.ts
+++ src/server.ts
@@ -8,16 +8,51 @@ interface DataParams{
 
 
 const teams = [
-    {id : 1, name : "McLaren", base : "Woking, United Kingdom"},
-    {id : 2, name : "Mercedes", base : "Brackley, United Kingdom"},
-    {id : 3, name : "Red Bull Racing", base : "Milton Keynes, United Kingdom"}
+    { id: 1, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
+    { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
+    { id: 3, name: "Ferrari", base: "Maranello, Italy" },
+    { id: 4, name: "McLaren", base: "Woking, United Kingdom" },
+    { id: 5, name: "Aston Martin", base: "Silverstone, United Kingdom" },
+    { id: 6, name: "Alpine", base: "Enstone, United Kingdom / Viry-Châtillon, France" },
+    { id: 7, name: "Williams", base: "Grove, United Kingdom" },
+    { id: 8, name: "RB (Visa Cash App RB)", base: "Faenza, Italy / Bicester, United Kingdom" },
+    { id: 9, name: "Sauber (Stake F1 Team Kick Sauber)", base: "Hinwil, Switzerland" },
+    { id: 10, name: "Haas", base: "Kannapolis, United States" }
 ];
 
+
 const drivers = [
-    {id : 1, name : "Max Verstappen", team : "Red Bull Racing"},
-    {id : 2, name : "Lewis Hamilton", team : "Ferrari"},
-    {id : 3, name : "Lando Norris", team : "McLaren"}
+    { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
+    { id: 2, name: "Sergio Pérez", team: "Red Bull Racing" },
+    
+    { id: 3, name: "Lewis Hamilton", team: "Ferrari" },
+    { id: 4, name: "Charles Leclerc", team: "Ferrari" },
+    
+    { id: 5, name: "George Russell", team: "Mercedes" },
+    { id: 6, name: "Andrea Kimi Antonelli", team: "Mercedes" },
+    
+    { id: 7, name: "Lando Norris", team: "McLaren" },
+    { id: 8, name: "Oscar Piastri", team: "McLaren" },
+    
+    { id: 9, name: "Fernando Alonso", team: "Aston Martin" },
+    { id: 10, name: "Lance Stroll", team: "Aston Martin" },
+    
+    { id: 11, name: "Esteban Ocon", team: "Alpine" },
+    { id: 12, name: "Pierre Gasly", team: "Alpine" },
+    
+    { id: 13, name: "Alexander Albon", team: "Williams" },
+    { id: 14, name: "Logan Sargeant", team: "Williams" },
+    
+    { id: 15, name: "Yuki Tsunoda", team: "RB (Visa Cash App RB)" },
+    { id: 16, name: "Daniel Ricciardo", team: "RB (Visa Cash App RB)" },
+    
+    { id: 17, name: "Valtteri Bottas", team: "Sauber (Stake F1 Team Kick Sauber)" },
+    { id: 18, name: "Zhou Guanyu", team: "Sauber (Stake F1 Team Kick Sauber)" },
+    
+    { id: 19, name: "Kevin Magnussen", team: "Haas" },
+    { id: 20, name: "Nico Hülkenberg", team: "Haas" }
 ];
+
 
 server.get("/teams", async (req, res) => {
     res.type("application/json");
